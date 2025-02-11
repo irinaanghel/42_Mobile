8f13fffde92288359156b694e2ef1115a9fa5187
Irina Anghel <anghel.irina99@gmail.com>
2025-02-11 19:01:14 +0100

    ex00 no build

---
+++ ex00/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: false,
+})
+export class AppComponent {
+  constructor() {}
+
+  showPress(){
+    console.log('Button pressed');
+  }
+}
